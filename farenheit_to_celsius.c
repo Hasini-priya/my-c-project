334b90081d185a5c7d56592174cde82a146871f5
Hasini-priya <24B01A0596@svecw.edu.in>
2024-10-15 17:09:54 +0530

    Add files via upload

---
+++ farenheit_to_celsius.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ main()
+{
+float celsius,farenheit;
+printf("enter the temperature ");
+scanf("%f",&farenheit);
+  celsius =(5.0/9)*(farenheit-32);
+  printf ("\n temperature in celsius is %f",celsius);
+  
+
+return 0;
+
+
+}
+    
